74276390fdd348375c70c86f4c624f9db0f599e0
KirillKKudr <kirillboss2109@gmail.com>
2025-02-22 12:52:15 +0200

    done

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const removeLastContact = async () => {
+    const contacts = await readContacts();
+    if (contacts.length > 0) {
+        contacts.pop();
+        await writeContacts(contacts);
+        console.log('Останній контакт видалено.');
+    } else {
+        console.log('Контактів немає.');
+    }
+};
 
 removeLastContact();
